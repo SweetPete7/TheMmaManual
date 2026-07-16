1dc086dab65d99721522dce2f986bad4c2b78c83
Peter Cunningham <39512529+SweetPete7@users.noreply.github.com>
2026-07-16 09:22:32 -0400

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MMAManualApp from './MMAManualApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MMAManualApp />
+  </React.StrictMode>
+)
