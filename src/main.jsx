6574a6ff4f9a85ecaf3aba18d7b07508e45ea391
Peter Cunningham <39512529+SweetPete7@users.noreply.github.com>
2026-07-16 10:54:08 -0400

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import MMAManualApp from './MMAManualApp.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MMAManualApp />
-  </React.StrictMode>
-)
